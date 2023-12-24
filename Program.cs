29bb72f0f3df55e83421834866f1ec56a60668ea
Shano Nithoer <shanonithoer@gmail.com>
2023-12-24 23:20:59 +0100

    Initial base code

---
+++ Program.cs
@@ -0,0 +1,149 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using VRage;
+using VRage.Audio;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        List<IMyShipController> controllers = new List<IMyShipController>();
+        List<IMyGasTank> tanks = new List<IMyGasTank>();
+
+        // Thrusters
+        List<IMyThrust> thrusters = new List<IMyThrust>();
+        List<IMyThrust> thrustersUp = new List<IMyThrust>();
+        List<IMyThrust> thrustersDown = new List<IMyThrust>();
+        List<IMyThrust> thrustersLeft = new List<IMyThrust>();
+        List<IMyThrust> thrustersRight = new List<IMyThrust>();
+
+        IMyShipController mainController = null;
+        Matrix mainControllerMatrix;
+        List<IMyGyro> gyros = new List<IMyGyro>();
+
+        double gravity = 0.0;
+        double mass = 0.0;
+        double altitude = 0.0;
+
+        public Program()
+        {
+            // The constructor, called only once every session and
+            // always before any other method is called. Use it to
+            // initialize your script. 
+            //     
+            // The constructor is optional and can be removed if not
+            // needed.
+            // 
+            // It's recommended to set Runtime.UpdateFrequency 
+            // here, which will allow your script to run itself without a 
+            // timer block.
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;
+
+            // Get main controller
+            GridTerminalSystem.GetBlocksOfType(controllers);
+            mainController = controllers.Find(x => x.CanControlShip);
+            mainController.Orientation.GetMatrix(out mainControllerMatrix);
+
+            // Get gyros 
+            GridTerminalSystem.GetBlocksOfType(gyros);
+
+            // Get thrusters
+            updateThrusters();
+        }
+
+        public void Save()
+        {
+            // Called when the program needs to save its state. Use
+            // this method to save your state to the Storage field
+            // or some other means. 
+            // 
+            // This method is optional and can be removed if not
+            // needed.
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            // The main entry point of the script, invoked every time
+            // one of the programmable block's Run actions are invoked,
+            // or the script updates itself. The updateSource argument
+            // describes where the update came from. Be aware that the
+            // updateSource is a  bitfield  and might contain more than 
+            // one update type.
+            // 
+            // The method itself is required, but the arguments above
+            // can be removed if not needed.
+
+            // TODO: Everything in try-catch block
+            if (mainController != null)
+            {
+                // Get gravity and mass
+                gravity = (double)mainController.GetNaturalGravity().Length();
+                mass = (double)mainController.CalculateShipMass().TotalMass;
+                altitude = (double)mainController.
+
+                Print("Gravity: " + gravity.ToString() +
+                    "\nMass: " + mass.ToString() +
+                    "\nTotal thrusters: " + thrusters.Count().ToString() +
+                    "\nTotal gyros: " + gyros.Count().ToString());
+
+            }
+        }
+
+        private void updateThrusters()
+        {
+            GridTerminalSystem.GetBlocksOfType(thrusters);
+
+            thrusters.ForEach(thruster =>
+            {
+                Matrix fromThrusterToGrid;
+                thruster.Orientation.GetMatrix(out fromThrusterToGrid);
+
+                Vector3 direction = Vector3.Transform(fromThrusterToGrid.Backward, mainControllerMatrix);
+
+                if (direction == mainControllerMatrix.Down)
+                {
+                    thrustersDown.Add(thruster);
+                }
+                else if (direction == mainControllerMatrix.Up)
+                {
+                    thrustersUp.Add(thruster);
+                }
+                else if (direction == mainControllerMatrix.Left)
+                {
+                    thrustersLeft.Add(thruster);
+                }
+                else if (direction == mainControllerMatrix.Right)
+                {
+                    thrustersRight.Add(thruster);
+                }
+            });
+        }
+
+        private void Print(String message)
+        {
+            IMyTextSurface mesurface0 = Me.GetSurface(0);
+            mesurface0.ContentType = ContentType.TEXT_AND_IMAGE;
+            mesurface0.FontSize = 2;
+            mesurface0.Alignment = VRage.Game.GUI.TextPanel.TextAlignment.CENTER;
+            mesurface0.WriteText(message);
+        }
+    }
+}
